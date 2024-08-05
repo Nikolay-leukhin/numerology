ba1340769f57bf5294298c98a9e8316380cf488a
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-08-05 14:22:27 +0300

    added svg

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"6e4deceb3802049388e167e3b14fda5843ad16e6","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"6e4deceb3802049388e167e3b14fda5843ad16e6","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "410861863"
+    serviceWorkerVersion: "2495744238"
   }
 });
--- lib/features/auth/data/auth_repository.dart
+++ lib/features/auth/data/auth_repository.dart
@@ -1,33 +1,33 @@
 import 'package:numerology/models/user.dart';
 import 'package:numerology/utils/enums.dart';
 import 'package:numerology/utils/exeptions.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:tele_web_app/tele_web_app.dart';
+// import 'package:rxdart/rxdart.dart';
+// import 'package:tele_web_app/tele_web_app.dart';
 
 class AuthRepository {
   UserModel? user;
 
-  final WebAppUser tgUser = TeleWebApp().initDataUnsafe.user!;
+  // final WebAppUser tgUser = TeleWebApp().initDataUnsafe.user!;
 
-  BehaviorSubject<AppAuthStateEnum> appState =
-      BehaviorSubject.seeded(AppAuthStateEnum.wait);
+  // BehaviorSubject<AppAuthStateEnum> appState =
+  //     BehaviorSubject.seeded(AppAuthStateEnum.wait);
 
-  AuthRepository() {
-    _checkIsAuthorize();
-  }
-
-  void _checkIsAuthorize(){
-    if(true) {
-      appState.add(AppAuthStateEnum.unAuth);
-    }
-    else{
-      appState.add(AppAuthStateEnum.auth);
-    }
-  }
-
-  registerWithTgId() {
-    appState.add(AppAuthStateEnum.auth);
-  }
+  // AuthRepository() {
+  //   _checkIsAuthorize();
+  // }
+  //
+  // void _checkIsAuthorize(){
+  //   if(true) {
+  //     appState.add(AppAuthStateEnum.unAuth);
+  //   }
+  //   else{
+  //     appState.add(AppAuthStateEnum.auth);
+  //   }
+  // }
+  //
+  // registerWithTgId() {
+  //   appState.add(AppAuthStateEnum.auth);
+  // }
 
   signInWithTgId() {}
 }
